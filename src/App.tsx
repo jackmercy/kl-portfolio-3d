3addd9d063f1c29ecd662b4274fb1e289344f8c0
Khoi <khoicm@anymindgroup.com>
2023-04-20 21:49:53 +0700

    add vercel analytic

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter } from 'react-router-dom';
 import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from './components';
 import Bonus from './components/Bonus';
+import { Analytics } from '@vercel/analytics/react';
 function App() {
 
   return (
@@ -22,8 +23,9 @@ function App() {
           <StarsCanvas />
           <Bonus />
         </div>
-        
+
       </div>
+      <Analytics />
     </BrowserRouter>
   )
 }
